1064ef6ede213d828285561ab4f9a45682784fda
Charlie Wager <charliewager@uvic.ca>
2022-01-27 13:09:52 -0800

    some refactoring

--- edit_album_list.py
+++ edit_album_list.py
@@ -1,32 +1,35 @@
 #!/usr/bin/env python3
 
 def edit():
-
     choice = (input("Options: add:album, remove:album, display\nWhat would you like to do? "))
 
     if choice[:3] == 'add':
-        
+
         print("Enter done when you are finished adding albums (no need for add: prefix)")
-        album_list = open("albums.txt", 'a')
-        albm_to_add = choice[4:]
-        album_list.write(albm_to_add + '\n')
+        album_file = open("albums.txt", 'a')
+        album_to_add = choice[4:]
+        album_file.write(album_to_add + '\n')
 
         while 1:
-            
+
             next_a = input()
             if next_a == 'done':
                 break
-            
-            album_list.write(next_a + '\n')
+
+            album_file.write(next_a + '\n')
 
     elif choice[:7] == 'display':
-        
-        album_list = open("albums.txt")
-        for line in album_list:
+
+        album_file = open("albums.txt")
+        for line in album_file:
             print(line)
-            
+
     else:
         print("NOT YET SUPPORTED, EXITING")
+        album_file = open("albums.txt")
+        line_list = []
+        for line in album_file:
+            line_list.append(line)
 
-    album_list.close()
+    album_file.close()
     return
