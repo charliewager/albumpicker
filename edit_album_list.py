5d86164e824c9e52ae6ad0b812bfba89f09ad9d2
Charlie Wager <charliewager@uvic.ca>
2022-01-27 15:57:55 -0800

    Updated README and implemented the remove feature

--- edit_album_list.py
+++ edit_album_list.py
@@ -1,22 +1,75 @@
 #!/usr/bin/env python3
 
+def add():
+    print("Enter done when you are finished adding albums (please provide album names in a name - artist format):")
+    album_file = open("albums.txt", 'a')
+
+    while 1:
+
+        next_addition = input()
+        if next_addition == 'done':
+            break
+
+        album_file.write(next_addition + '\n')
+
+    album_file.close()
+
+
+def remove():
+
+    print("Enter done when you are finished removing albums (provide names in the same format you added them in):")
+    album_file = open("albums.txt")
+    line_list = []
+    for line in album_file:
+        line_list.append(line)
+
+    album_file.close()
+
+    to_remove = []
+    count = 0
+    individual = input()
+    while individual != 'done':
+        to_remove.append(individual)
+        individual = input()
+
+    updated_list = []
+    for line in line_list:
+        lastIDX = len(line) - 1
+
+        action = 0
+        for removal in to_remove:
+            if line[:lastIDX] == removal:
+                action = 1
+
+        if action == 0:
+            updated_list.append(line)
+        else:
+            count += 1
+
+    i = 0
+    for line in updated_list:
+
+        if i == 0:
+            album_file = open("albums.txt", "w")
+            album_file.write(line)
+            album_file.close()
+
+        else:
+            album_file = open("albums.txt", "a")
+            album_file.write(line)
+            album_file.close()
+
+        i += 1
+
+    print("removed ", count, " items in total")
+
+
 def edit():
-    choice = (input("Options: add:album, remove:album, display\nWhat would you like to do? "))
+    choice = (input("Options: add, remove, display\nWhat would you like to do? "))
 
     if choice[:3] == 'add':
 
-        print("Enter done when you are finished adding albums (no need for add: prefix)")
-        album_file = open("albums.txt", 'a')
-        album_to_add = choice[4:]
-        album_file.write(album_to_add + '\n')
-
-        while 1:
-
-            next_a = input()
-            if next_a == 'done':
-                break
-
-            album_file.write(next_a + '\n')
+        add()
 
     elif choice[:7] == 'display':
 
@@ -24,12 +77,12 @@ def edit():
         for line in album_file:
             print(line)
 
-    else:
-        print("NOT YET SUPPORTED, EXITING")
-        album_file = open("albums.txt")
-        line_list = []
-        for line in album_file:
-            line_list.append(line)
+        album_file.close()
 
-    album_file.close()
+    elif choice[:6]:
+
+        remove()
+
+    else:
+        print("please enter either add, remove, or display")
     return
